f64c7b5b06aaa446d84b0d8a0fe34be2c98682fb
Jodi Tivis <joditivis@gmail.com>
2019-10-30 15:38:27 -0600

    trying to remove every tag for each question and handle each of them in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
-var timerNumber;
 var rightAnswers = 0;
 var wrongAnswers = 0;
 var unanswered = 0;
+var interval;
+var timerNumber = 300;
 
 
 var gameQuestions = [{
@@ -80,18 +81,87 @@ var gameQuestions = [{
             correctAnswer: "Germany"
         }];
 
-//START FUNCTION
-$("#start-button").on("click", function() {
 
-    //hides start button after click
-    $("#start-button").hide();
+    $("#start-button").on("click", function() {
 
-    // display time Count
-    $("timer").html("<h2>Time: 5 minutes</h2>" + "<br>");
+        //hides start page and start button after clicked
+        $("#start-page").hide();
+
+        startTimer();
+        
+
+        var questionsPage = $("#questions-page");
+
+        //adds the following words to screen when start button is pressed
+        questionsPage.append('<h2>Answer the following questions: </h2>');
+
+    //loops and prints all of the questions in the array
+        for (var i = 0; i < gameQuestions.length; i++) {
+
+            questionsPage.append(gameQuestions[i].question.answerChoices);
+
+            var answer1 = gameQuestions[i].answerChoices[0];
+            var answer2 = gameQuestions[i].answerChoices[1];
+            var answer3 = gameQuestion[i].answerChoices[2];
+            var answer4 = gameQuestions[i].answerChoices[3];
+
+            questionsPage.append()
+            questionsPage.append
+            questionsPage.append
+            questionsPage.append
+        };
+    });
+
+        $("#done-button").on("click", function() {
+
+            scoreKeeper();
+            showEndPage();
+        });
+
+//starts timer
+    function startTimer() {
+        
+        clearInterval(interval);
+        interval = setInterval(decrement, 1000);
+    }
+
+//decreases the amount of time on the timer
+    function decrement() {
+        timerNumber--;
+        $("#timer").html("<h2>Time: " + timerNumber + "</h2>");
+
+        if (timerNumber === 0) {
+            stopGame();
+
+            scoreKeeper();
+            showEndPage();
+        }
+    }
+
+    //stops timer and clears it if runs out or game is finished
+    function stopGame() {
+        clearInterval(interval);
+    }
+
+    function showEndPage() {
+
+        //hides all questions
+        $("#question-page").hide();
+
+        //shows end page with results
+        $("#results-text").text("<h3>All done! Here are your results: </h2>");
+        $("#right-answers").text("Correct: " + rightAnswers);
+        $("#wrong-answers").text("Incorrect: " + wrongAnswers);
+        $("#unanswered-questions").text("Unanswered: " + unanswered);
+    }
 
 
 
-})
+
+});
 
 
-})
+// ('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer1 + '</label></div>');
+//             questionsPage.append('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer2 + '</label></div>');
+//             questionsPage.append('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer3 + '</label></div>');
+//             questionsPage.append('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer4 + '</label></div>');
