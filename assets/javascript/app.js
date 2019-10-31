2f4a14ed792899048940efb634a8e8a68408c08e
Jodi Tivis <joditivis@gmail.com>
2019-10-31 16:32:22 -0600

    adding timer and buttons to javascript and css style to done button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,113 +10,119 @@ var timerNumber = 300;
 var gameQuestions = [{
             question: "What year were Christmas lights invented?",
             answerChoices: ["1793", "1910", "1882", "1837"],
-            correctAnswer: "1882"
+            correctAnswer: 2 //1882 is at index 2
         },{
             question: "What is the highest selling Christmas song?",
             answerChoices: ["White Christmas", "Jingle Bells", "Silent Night", "Carol of the Bells"],
-            correctAnswer: "White Christmas"
+            correctAnswer: 0 //white christmas is at index 0
         },{
             question: "Which of these companies was the first to use Santa Claus in an advertisement?",
             answerChoices: ["Pepsi", "Coca-Cola", "Dr. Pepper", "Sprite"],
-            correctAnswer: "Coca-Cola"
+            correctAnswer: 1 // coca-cola is at index 1
         },{
             question: "Which president was the first to decorate the White House Christmas tree?",
             answerChoices: ["Benjamin Franklin", "George Washington", "Abraham Lincoln", "Franklin Pierce"],
-            correctAnswer: "Franklin Pierce"
+            correctAnswer: 3 // franklin pierce is at index 3
         },{
             question: "Which country did the ginger bread house come from?",
             answerChoices: ["Austria", "Germany", "Switzerland", "United States"],
-            correctAnswer: "Germany"
+            correctAnswer: 1 // germany is at index 1
         },{
             question: "In what year was A Christmas Carol by Charles Dickens published?",
             answerChoices: ["1765", "1843", "1860", "1906"],
-            correctAnswer: "1843"
+            correctAnswer: 1 // 1843 is at index 1
         },{
             question: "How many reindeer pull Santa's sleigh?",
             answerChoices: ["8", "9", "10", "12"],
-            correctAnswer: "8"
+            correctAnswer: 0 // 8 is at index 0
         },{
             question: "Who decided the date for Christmas would be December 25th?",
             answerChoices: ["Pope John Paul", "Emporor Constantine", "Pope Julius I", "Pope Clement V"],
-            correctAnswer: "Pope Julius I"
+            correctAnswer: 2 // pope julius I is at index 2
         },{
             question: "What do traditional Ukrainians put on their Christmas tree for good luck?",
             answerChoices: ["candles", "a bird's nest", "goose feathers", "a spider's web"],
-            correctAnswer: "a spider's web"
+            correctAnswer: 3 // a spiders web is at index 3
         },{
             question: "After leaving Bethlehem, which country did Joseph, Mary, and Jesus travel to?",
             answerChoices: ["Egypt", "Jordan", "Lebanon", "Syria"],
-            correctAnswer: "Egypt"
+            correctAnswer: 0 // egypt is at index 0
         },{
             question: "Which country created eggnog?",
             answerChoices: ["Switzerland", "Germany", "England", "Poland"],
-            correctAnswer: "England"
+            correctAnswer: 2 // england is at index 2
         },{
             question: "What is the most popular ornament for the top of the Christmas tree?",
             answerChoices: ["Star", "Bow", "Dove", "Angel"],
-            correctAnswer: "Angel"
+            correctAnswer: 3 // angel is at index 3
         },{
             question: "Which state in America was the first to recognize Christmas as an official holiday?",
             answerChoices: ["Alabama", "Texas", "New York", "Washington"],
-            correctAnswer: "Alabama"
+            correctAnswer: 0 // alabama is at index 0
         },{
             question: "What was the last ghost called in A Christmas Carol?",
             answerChoices: ["The Ghost of Christmas Present", "The Ghost of Christmas Yet to Come", "The Ghost of Christmas Past", "Jacob Marley's Ghost"],
-            correctAnswer: "The Ghost of Christmas Yet to Come"
+            correctAnswer: 1  // "The Ghost of Christmas Yet to Come" is at index 1
         },{
             question: "What holiday film annually plays on tv more than 300 times?",
             answerChoices: ["A Christmas Story", "The Grinch", "It's a Wonderful Life", "Elf"],
-            correctAnswer: "It's a Wonderful Life"
+            correctAnswer: 2 //"It's a Wonderful Life" is at index 2
         },{
             question: "How many pipers piping did my true love give to me?",
             answerChoices: ["7", "11", "9", "4"],
-            correctAnswer: "11"
+            correctAnswer: 1 // 11 is at index 1
         },{
             question: "What country started the tradition of exchanging gifts?",
             answerChoices: ["Italy", "Germany", "Greece", "Israel"],
-            correctAnswer: "Italy"
+            correctAnswer: 0 // Italy is at index 0
         },{
             question: "What country started the tradition of putting up a Christmas tree?",
             answerChoices: ["Ukraine", "Russia", "Germany", "Norway"],
-            correctAnswer: "Germany"
+            correctAnswer: 2 // germany is at index 2
         }];
-
 
     $("#start-button").on("click", function() {
 
         //hides start page and start button after clicked
         $("#start-page").hide();
 
+        //calls start timer function
         startTimer();
         
-
+        //creating a variable for our questions page
         var questionsPage = $("#questions-page");
+        var questionsPageOpener = $("#questions-page-opener");
 
         //adds the following words to screen when start button is pressed
-        questionsPage.append('<h2>Answer the following questions: </h2>');
+        questionsPageOpener.append('<h3>Answer the following questions:</h3>');
 
     //loops and prints all of the questions in the array
         for (var i = 0; i < gameQuestions.length; i++) {
 
-            questionsPage.append(gameQuestions[i].question.answerChoices);
+            //this displays all questions to the page
+            questionsPage.append('<div id="question">' + gameQuestions[i].question + '</div>');
 
+            //variables for each answer option
             var answer1 = gameQuestions[i].answerChoices[0];
             var answer2 = gameQuestions[i].answerChoices[1];
-            var answer3 = gameQuestion[i].answerChoices[2];
+            var answer3 = gameQuestions[i].answerChoices[2];
             var answer4 = gameQuestions[i].answerChoices[3];
 
-            questionsPage.append()
-            questionsPage.append
-            questionsPage.append
-            questionsPage.append
-        };
-    });
+            //this displays all answer options to page and displays them with a selector
+            questionsPage.append("<input type='radio' name='answers' value='0'>" + "<label>" + answer1 + "</label>");
+            questionsPage.append("<input type='radio' name='answers' value='0'>" + "<label>" + answer2 + "</label>");
+            questionsPage.append("<input type='radio' name='answers' value='0'>" + "<label>" + answer3 + "</label>");
+            questionsPage.append("<input type='radio' name='answers' value='0'>" + "<label>" + answer4 + "</label>");
+       
+        }
 
-        $("#done-button").on("click", function() {
-
-            scoreKeeper();
+        // var doneButton = ('<button class="btn btn-danger btn-lg" id="done" type="submit">Done</button>');
+        questionsPage.append("<button class='btn btn-danger btn-lg' id='done'>Done</button>");
+        $("#done").on("click", function() {
+            stopGame();
             showEndPage();
         });
+    });
 
 //starts timer
     function startTimer() {
@@ -128,17 +134,26 @@ var gameQuestions = [{
 //decreases the amount of time on the timer
     function decrement() {
         timerNumber--;
-        $("#timer").html("<h2>Time: " + timerNumber + "</h2>");
+
+        var timeConverted = time_converter();
+        $("#timer").text(timeConverted);
 
         if (timerNumber === 0) {
             stopGame();
-
-            scoreKeeper();
             showEndPage();
         }
     }
 
-    //stops timer and clears it if runs out or game is finished
+
+    var timeConverted = time_converter(num);
+
+    function time_converter(num) { 
+      var minutes = Math.floor(num / 60);  
+      var seconds = num % 60;
+      return minutes + ":" + seconds;         
+}
+
+    //stops timer and clears it if runs out or user is done
     function stopGame() {
         clearInterval(interval);
     }
@@ -146,22 +161,14 @@ var gameQuestions = [{
     function showEndPage() {
 
         //hides all questions
-        $("#question-page").hide();
+        $("#questions-page").hide();
 
         //shows end page with results
-        $("#results-text").text("<h3>All done! Here are your results: </h2>");
+        $("#results-text").html("<h3>All done! Here are your results: </h2>");
         $("#right-answers").text("Correct: " + rightAnswers);
         $("#wrong-answers").text("Incorrect: " + wrongAnswers);
         $("#unanswered-questions").text("Unanswered: " + unanswered);
     }
 
 
-
-
 });
-
-
-// ('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer1 + '</label></div>');
-//             questionsPage.append('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer2 + '</label></div>');
-//             questionsPage.append('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer3 + '</label></div>');
-//             questionsPage.append('<div class="answer-check"><input class="answer-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="answer-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer4 + '</label></div>');
