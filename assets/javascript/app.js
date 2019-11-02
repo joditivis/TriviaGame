2a9fd503c65c3f0babfa122bbd310660db1fb860
Jodi Tivis <joditivis@gmail.com>
2019-11-02 02:43:05 -0600

    still trying to figure out how to log answer results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,75 +14,75 @@ var timerNumber = 300;
 var gameQuestions = [{
             question: "What year were Christmas lights invented?",
             answerChoices: ["1793", "1910", "1882", "1837"],
-            correctAnswer: 2 //1882 is at index 2
+            correctAnswer: "1882" //1882 is at index 2
         },{
             question: "What is the highest selling Christmas song?",
             answerChoices: ["White Christmas", "Jingle Bells", "Silent Night", "Carol of the Bells"],
-            correctAnswer: 0 //white christmas is at index 0
+            correctAnswer: "White Christmas" //white christmas is at index 0
         },{
             question: "Which of these companies was the first to use Santa Claus in an advertisement?",
             answerChoices: ["Pepsi", "Coca-Cola", "Dr. Pepper", "Sprite"],
-            correctAnswer: 1 // coca-cola is at index 1
+            correctAnswer: "Coca-Cola" // coca-cola is at index 1
         },{
             question: "Which president was the first to decorate the White House Christmas tree?",
             answerChoices: ["Benjamin Franklin", "George Washington", "Abraham Lincoln", "Franklin Pierce"],
-            correctAnswer: 3 // franklin pierce is at index 3
+            correctAnswer: "Franklin Pierce" // franklin pierce is at index 3
         },{
             question: "Which country did the ginger bread house come from?",
             answerChoices: ["Austria", "Germany", "Switzerland", "United States"],
-            correctAnswer: 1 // germany is at index 1
+            correctAnswer: "Germany" // germany is at index 1
         },{
             question: "In what year was A Christmas Carol by Charles Dickens published?",
             answerChoices: ["1765", "1843", "1860", "1906"],
-            correctAnswer: 1 // 1843 is at index 1
+            correctAnswer: "1843" // 1843 is at index 1
         },{
             question: "How many reindeer pull Santa's sleigh?",
             answerChoices: ["8", "9", "10", "12"],
-            correctAnswer: 0 // 8 is at index 0
+            correctAnswer: "8" // 8 is at index 0
         },{
             question: "Who decided the date for Christmas would be December 25th?",
             answerChoices: ["Pope John Paul", "Emporor Constantine", "Pope Julius I", "Pope Clement V"],
-            correctAnswer: 2 // pope julius I is at index 2
+            correctAnswer: "Pope Julius I" // pope julius I is at index 2
         },{
             question: "What do traditional Ukrainians put on their Christmas tree for good luck?",
             answerChoices: ["candles", "a bird's nest", "goose feathers", "a spider's web"],
-            correctAnswer: 3 // a spiders web is at index 3
+            correctAnswer: "a spider's web" // a spiders web is at index 3
         },{
             question: "After leaving Bethlehem, which country did Joseph, Mary, and Jesus travel to?",
             answerChoices: ["Egypt", "Jordan", "Lebanon", "Syria"],
-            correctAnswer: 0 // egypt is at index 0
+            correctAnswer: "Egypt" // egypt is at index 0
         },{
             question: "Which country created eggnog?",
             answerChoices: ["Switzerland", "Germany", "England", "Poland"],
-            correctAnswer: 2 // england is at index 2
+            correctAnswer: "England" // england is at index 2
         },{
             question: "What is the most popular ornament for the top of the Christmas tree?",
             answerChoices: ["Star", "Bow", "Dove", "Angel"],
-            correctAnswer: 3 // angel is at index 3
+            correctAnswer: "Angel" // angel is at index 3
         },{
             question: "Which state in America was the first to recognize Christmas as an official holiday?",
             answerChoices: ["Alabama", "Texas", "New York", "Washington"],
-            correctAnswer: 0 // alabama is at index 0
+            correctAnswer: "Alabama" // alabama is at index 0
         },{
             question: "What was the last ghost called in A Christmas Carol?",
             answerChoices: ["The Ghost of Christmas Present", "The Ghost of Christmas Yet to Come", "The Ghost of Christmas Past", "Jacob Marley's Ghost"],
-            correctAnswer: 1  // "The Ghost of Christmas Yet to Come" is at index 1
+            correctAnswer: "The Ghost of Christmas Yet to Come"  // "The Ghost of Christmas Yet to Come" is at index 1
         },{
             question: "What holiday film annually plays on tv more than 300 times?",
             answerChoices: ["A Christmas Story", "The Grinch", "It's a Wonderful Life", "Elf"],
-            correctAnswer: 2 //"It's a Wonderful Life" is at index 2
+            correctAnswer: "It's a Wonderful Life" //"It's a Wonderful Life" is at index 2
         },{
             question: "How many pipers piping did my true love give to me?",
             answerChoices: ["7", "11", "9", "4"],
-            correctAnswer: 1 // 11 is at index 1
+            correctAnswer: "11" // 11 is at index 1
         },{
             question: "What country started the tradition of exchanging gifts?",
             answerChoices: ["Italy", "Germany", "Greece", "Israel"],
-            correctAnswer: 0 // Italy is at index 0
+            correctAnswer: "Italy" // Italy is at index 0
         },{
             question: "What country started the tradition of putting up a Christmas tree?",
             answerChoices: ["Ukraine", "Russia", "Germany", "Norway"],
-            correctAnswer: 2 // germany is at index 2
+            correctAnswer: "Germany" // germany is at index 2
         }];
 
 
@@ -151,7 +151,7 @@ var gameQuestions = [{
         var timeConverted = time_converter(timerNumber);
 
         //displays the timer to the page
-        $("#timer").html('<h2>Time: ' + timeConverted + '</h2>');
+        $("#timer").html('<h2>Time: ' + timeConverted + '</h2><br>');
 
         // if the time equals zero it will take player to results end page
         if (timerNumber === 0) {
@@ -208,11 +208,9 @@ var gameQuestions = [{
             userChoice = $('input[id=radio'+i+']:checked').text();
             if (userChoice === correctChoice) {
               rightAnswers++;
-            } 
-            else if (userChoice !== correctChoice) {
+            } else if (userChoice !== correctChoice) {
                 wrongAnswers++;
-            }
-            else if (userChoice === "") {
+            } else if (userChoice === "") {
                 unanswered++;
             } 
         }
